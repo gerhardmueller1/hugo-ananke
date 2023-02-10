42a4d4ab59b95ed2688702418baa122a4d9672eb
gerd <gerd@devel.fritz.box>
2023-02-10 17:36:52 +0100

    update script

--- Update_from_NewDynamic.sh
+++ Update_from_NewDynamic.sh
@@ -1,1 +1,13 @@
 git remote add upstream https://github.com/theNewDynamic/gohugo-theme-ananke.git
+git fetch upstream
+git merge upstream
+
+sleep 10
+
+clear
+echo "##################################################"
+echo "please revert changes by "
+echo "   git revert ...."
+echo "   git restore ...."
+echo "##################################################"
+
